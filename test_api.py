521b80c8d48c0c9a396f3d7f1cfcb44f21d8e0f0
kangdong <k.jin@tu-braunschweig.de>
2024-09-23 11:32:40 +0200

    write some unit test cases for API

---
+++ test_api.py
@@ -0,0 +1,41 @@
+import unittest
+import json
+from api import app
+
+
+class TestAPI(unittest.TestCase):
+    """
+    Test cases on the segment matching API for checking correctness, functionality
+    """
+    def setUp(self):
+        """ Set up the test environment """
+        self.app = app.test_client()  # open a flask API test client
+        self.app.testing = True
+        with open('./test_audiences.json', 'r') as f:
+            self.test_data = json.load(f)
+
+    def test_valid_data(self):
+        """ Valid input test """
+        response = self.app.post('/match',
+                                 data=json.dumps(self.test_data),
+                                 content_type='application/json'
+                                 )
+        data = json.loads(response.data)
+        print(data)
+
+    def test_empty_data(self):
+        """ Empty input test"""
+        response = self.app.post('/match',
+                                 data=json.dumps({"input_segments": []}),
+                                 content_type='application/json')
+        self.assertEqual(response.status_code, 400)  # should return 400 for empty input
+        data = json.loads(response.data)
+        print(data)
+
+    # TODO: more test cases for other scenarios, like large scale input ...
+    # def test_large_scale_input(self):
+    #     """ Large input test """
+
+
+if __name__ == '__main__':
+    unittest.main()
